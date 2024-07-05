cab0952edb88104c1a2f9f0b487afdf17edc18c1
Thomas Almeida <contato.thomasalmeidard@gmail.com>
2024-07-05 16:27:24 -0300

    creating lp

---
+++ src/scroll-down.js
@@ -0,0 +1,13 @@
+// Get the button
+let mybutton = document.getElementById("btn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "flex";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
